f182ce89af795ae8d2b976432e1e0adb6c296f84
Jeteco <prateek0426@gmail.com>
2023-11-27 02:58:25 +0530

    changes done

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Nav from "@/components/Nav"
 import Provider from "@/components/Provider"
 
 export const metadata = {
-  title: "FAQ",
+  title: "ShareHub",
   description: "Best place to get Answers"
 }
 
